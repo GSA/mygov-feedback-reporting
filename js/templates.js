0b3ce45e4e0a41bd15ad51186c69f033a76e9460
Benjamin J. Balter <ben@balter.com>
2013-01-17 12:26:06 -0500

    show num_ratings

--- js/templates.js
+++ js/templates.js
@@ -53,7 +53,9 @@ __p+='<h2>'+
 ( page.url )+
 '</li>\n  <li><strong>Average Rating:</strong> '+
 ( page.avg_rating )+
-'</li>\n  <li><strong>Domain:</strong> <a href="#domains/'+
+' ('+
+( page.num_rating )+
+')</li>\n  <li><strong>Domain:</strong> <a href="#domains/'+
 ( page.domain.id )+
 '">'+
 ( page.domain.hostname )+
